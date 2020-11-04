3f70331b352567c174f089a0d4bf2f572d00b0ef
andy <arteysoft@gmail.com>
2020-11-04 15:55:05 -0300

    base de datos 1

---
+++ clase7/basededatos/src/main/java/edu/it/utiles/Utiles.java
@@ -0,0 +1,25 @@
+package edu.it.utiles;
+
+import java.util.Scanner;
+
+public class Utiles {
+    public static boolean quiereSeguirJugando() {
+        System.out.println(System.lineSeparator());
+        System.out.println("Este juego ha conlcuido !!!!");
+        System.out.println("Presione 1 si quiere continuar con otro juego !!!");
+        System.out.println(System.lineSeparator());
+        Scanner teclado = new Scanner(System.in);
+        int seleccionUsuario = teclado.nextInt();
+        System.out.println(System.lineSeparator());
+        if (seleccionUsuario==1) {
+            return true;
+        }
+        return false;
+    }
+    public static void ver(Object ...params) {
+        for (Object o : params) {
+            System.out.print(o);
+        }
+        System.out.println();
+    }
+}
